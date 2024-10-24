2d882950b8303e24512db598871e87681eac1730
Santosh-Rashinkar <145894311+Santosh-Rashinkar@users.noreply.github.com>
2024-10-24 22:53:12 +0530

    Add files via upload
    Remove Duplicates element

--- src/main/java/com/prowings/RemoveDuplicates.java
+++ src/main/java/com/prowings/RemoveDuplicates.java
@@ -15,5 +15,18 @@ public class RemoveDuplicates {
                 .collect(Collectors.toList());
 
         System.out.println("Unique fruits : " + uniqueFruites);
+
+        List<Integer> numbers = Arrays.asList(10,20,30,40,50,60,70,20,40,60);
+
+        removeDuplicatesElements(numbers);
+    }
+
+    public static void removeDuplicatesElements(List<Integer> nums){
+        
+        List<Integer> uniqueNumbers = nums.stream()
+                .distinct()
+                .collect(Collectors.toList());
+
+        System.out.println("Unique Numbers : " + uniqueNumbers);
     }
 }
